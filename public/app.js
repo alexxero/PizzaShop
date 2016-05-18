6b38bb3ac9b2c2de58d5abfddbc20474e79e87f0
alexxero <alexxero@mail.ru>
2016-05-18 13:06:27 +0300

    Updated add_to_cart function

--- public/app.js
+++ public/app.js
@@ -6,5 +6,9 @@ function something() {
 }
 
 function add_to_cart(id) {
-    alert ("Added pizza with id " +id + "!");
+    var key = 'product_' + id;
+    var x = window.localStorage.getItem(key);
+
+    x = x * 1 + 1;
+    window.localStorage.setItem(key, x);
 }
