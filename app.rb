f97c7509659a3d5aff79e25c18e9c2adb157d078
alexxero <alexxero@mail.ru>
2016-05-24 19:57:02 +0300

    Displaying array on cart page

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
-set :database, "sqlite3:pizzashop.db"
+set :database, 'sqlite3:pizzashop.db'
 
 class Product < ActiveRecord::Base
 
@@ -24,7 +24,25 @@ get '/cart' do
 end
 
 post '/cart' do
- erb "Hello World"
+  orders_input = params[:orders]
+  @orders = pars_orders_line orders_input
+ erb "#{@orders.inspect} Test"
+end
+
+def pars_orders_line orders_input
+
+  s1 = orders_input.split(",")
+  arr = []
+  s1.each {|x|
+    s2 = x.split("=")
+    s3 = s2[0].split("_")
+    id = s3[1]
+    cnt = s2[1]
+
+    arr2 = [id, cnt]
+    arr.push arr2
+  }
+  arr
 end
 
 after do
