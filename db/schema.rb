50724628649de58a6fc59d61805842e315c570c6
alexxero <alexxero@mail.ru>
2016-05-25 11:50:25 +0300

    New table Orders + migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160517170809) do
+ActiveRecord::Schema.define(version: 20160525084711) do
+
+  create_table "orders", force: :cascade do |t|
+    t.text     "name"
+    t.text     "phone"
+    t.text     "address"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
